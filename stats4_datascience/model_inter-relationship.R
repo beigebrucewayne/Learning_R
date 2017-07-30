aa0287e847868973af16d18510f9c7d6e200856b
kade killary <kadekillary@protonmail.com>
2017-07-29 21:27:39 -0700

    segmentation and inter-relationship

---
+++ stats4_datascience/model_inter-relationship.R
@@ -0,0 +1,51 @@
+# inter-relationship techniques understand how variables interact together
+
+# segmentation - sub-dividing population into similar sub-markets
+	# homogeneous within and heterogeneous between
+
+# 3 uses of segmentation
+	# finding similar members
+	# make modelling better
+	# use marketing strategy to attack each segment differently
+
+# Marketing research - learning WHY. Segmentation as rationale for behavour.
+# Marketing strategy - targeting by product, price, promotion and place. Strategy uses the marketing mix by exploiting segment differences.
+# Marketing communications - messaging and positioning. One size doesn't fit all, different segments need different messaging.
+# Marketing economics - imperfect competition leads to price makers. Right product, right price, right time, right channel = nearly monopolistic power.
+
+# Four P's of strategic marketing
+
+# Partition
+	# apply segmentatio algorithm to divide market into sub-markets
+
+# Probe
+	# additional data
+		# created vars, tend to be around velocity, (time between purchase, share of products penetrated, seasonality, consumer confidence)
+
+# Prioritize
+	# financial analysis of resulting segments
+		# most profitable, growing fastest, require most effort to keep or cost to serve
+		# find customers not worth pursuing
+
+# Position
+	# using all above insights + appropriate message
+
+
+# Criteria for actionable segmentation
+
+	# Identifiability - segment has to be identifiable. Scoring database with each customer having a probability of belonging to each segment
+	# Substantiality - each segment needs to be substantial enough to make marketing to it worthwhile
+	# Acessibility - way to get to them via marketing efforts
+	# Stability - segment membership should not change drastically
+	# Responsiveness - segmentation must drive responses
+
+# Managerial judgements for segmentation
+	# usage, profit, satisfaction, size, growth
+	# a univariate approach to what is a multivariate problem
+
+# Most important customer metric? 
+	# number of purchases, rev, visits, etc..
+
+# Profiling
+	# show means of each KPI by segment
+	# indexes - take each segment's mean / total mean
